0613b179df3b2adc85ae34aa2ac59325b0970706
Diego Santamaria <jd.santamariab1@uniandes.edu.co>
2025-09-25 04:41:53 -0500

    feat: modify laboral week

--- movil/app/src/main/java/com/jdsant/festivalarm/ManageCalendar.kt
+++ movil/app/src/main/java/com/jdsant/festivalarm/ManageCalendar.kt
@@ -14,10 +14,16 @@ class ManageCalendar : AppCompatActivity() {
         setContentView(R.layout.activity_manage_calendar)
 
       val modificarFestivos = findViewById<TextView>(R.id.modifyHolidays)
+      val modificarLaboral = findViewById<TextView>(R.id.modifyWeek)
       val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
       modificarFestivos.setOnClickListener {
         val intent = Intent(this, ModifyCalendar::class.java)
+        startActivity(intent)
+      }
+
+      modificarLaboral.setOnClickListener {
+        val intent = Intent(this, ModifyLaboralWeek::class.java)
         startActivity(intent)
       }
 
